398057c66eb9c67e06f308cf2578a26b1c284116
omateusborba <RM556418@fiap.com.br>
2025-07-22 16:12:29 -0300

    Manutenção do código e alteração do DB

--- Back-end/src/main/java/org/example/factory/Factory.java
+++ Back-end/src/main/java/org/example/factory/Factory.java
@@ -2,9 +2,9 @@ package org.example.factory;
 
 import java.sql.*;
 public class Factory {
-    private static final String URL = "jdbc:postgresql://db-teste.cjq6s846y3u1.sa-east-1.rds.amazonaws.com:5432/Flux-ERP";
+    private static final String URL = "jdbc:postgresql://db.fmcxxrvrksohyruomhdf.supabase.co:5432/postgres";
     private static final String USER = "postgres";
-    private static final String SENHA = "FluxERP321$";
+    private static final String SENHA = "FluX3RPFIAP";
     // Metodo para obter uma conexão com o banco de dados
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(URL, USER, SENHA);
