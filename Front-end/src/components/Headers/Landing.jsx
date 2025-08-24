e0274056c2ca0e64d44367a29387fc9093155f38
FlintMorais <pedromoraisaugusto7@gmail.com>
2025-08-24 20:04:55 -0300

    ajustes landingpage

--- Front-end/src/components/Headers/Landing.jsx
+++ Front-end/src/components/Headers/Landing.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
 import logo from '../img/flux-logo.png';
-import perfil from "../img/user-1.jpg";
 export default function Landing() {
   return (
     <>
 <header className="app-header border-bottom">
-  <nav className="navbar navbar-expand-lg navbar-light">
-    <a className="navbar-brand" href="#">
-      <Link to="/">
-        <img src={logo} alt="flux-logo" width={80} className="logo-flux tittle" />
+  <nav className="navbar navbar-expand-lg navbar-light container-fluid">
+    <Link to="/" className="navbar-brand">
+      <img src={logo} alt="flux-logo" width={80} />
+    </Link>
+
+    <div className="ms-auto mx-4">
+      <Link to="/login" className="btn btn-primary">
+        Cadastre-se
       </Link>
-    </a>
-      </nav>
-    </header>
+    </div>
+  </nav>
+</header>
 
     </>
   );
